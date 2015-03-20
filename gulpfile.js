27fa86f3089a2983546d4321b9eaae31d47477cf
Marco Godínez <markotom@gmail.com>
2015-03-20 00:56:38 -0600

    Added needed gulp tasks
    Compile and minify styles, concat and uglify scripts, generate zip
    file, and one task to watch files and run browsersync

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+var browserSync = require('browser-sync'),
+    reload      = browserSync.reload,
+    gulp        = require('gulp'),
+    less        = require('gulp-less'),
+    minifyCss   = require('gulp-minify-css'),
+    concat      = require('gulp-concat'),
+    uglify      = require('gulp-uglify'),
+    zip         = require('gulp-zip'),
+    pkg         = require('./package.json');
+
+// Browser Sync
+gulp.task('browser-sync', function () {
+	browserSync({
+		open: false,
+		proxy: 'localhost/wordpress'
+	});
+});
+
+// Development styles
+gulp.task('styles:development', function () {
+	return gulp.src('./assets/less/styles.less')
+		.pipe(less({ paths: './assets' }))
+		.pipe(gulp.dest('./built/css'))
+		.pipe(reload({ stream: true }));
+});
+
+// Production styles
+gulp.task('styles:production', function () {
+	return gulp.src('./assets/less/styles.less')
+		.pipe(less({ paths: './assets' }))
+		.pipe(minifyCss())
+		.pipe(gulp.dest('./built/css'));
+});
+
+// Build app scripts
+gulp.task('scripts:app', function() {
+	return gulp.src('./assets/js/**/*.js')
+		.pipe(concat('bdpn.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('./built/js'))
+		.pipe(reload({ stream: true }));
+});
+
+// Build Wordpress Theme
+gulp.task('theme', [
+	'styles:production',
+	'scripts:app'
+], function () {
+	return gulp.src([
+			'*.php',
+			'style.css',
+			'screenshot.jpg',
+			'built/**/*',
+			'includes/**/*',
+			'templates/**/*'
+		], { base: './' })
+		.pipe(zip(pkg.name + '-' + pkg.version + '.zip'))
+		.pipe(gulp.dest('./'));
+});
+
+// Default development task
+gulp.task('default', [
+	'styles:development',
+	'scripts:app',
+	'browser-sync'
+], function () {
+	gulp.watch('./**/*.php', reload);
+	gulp.watch('./assets/less/**/*.less', ['styles:development']);
+	gulp.watch('./assets/js/**/*.js', ['scripts:app']);
+});
