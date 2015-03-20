1382a30e0ec7ad4ffbb000f141edf54865eff095
Marco Godínez <markotom@gmail.com>
2015-03-20 00:13:36 -0600

    first commit

---
+++ header.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html class="no-js" <?php language_attributes(); ?>>
+<head>
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
