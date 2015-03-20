01b5d44c5ee0a5d5dbafad96e9405537c7eeee06
Marco Godínez <markotom@gmail.com>
2015-03-20 00:42:35 -0600

    Added html5 support on head

--- header.php
+++ header.php
@@ -5,6 +5,9 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0">
+	<!--[if lt IE 9]>
+		<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
 
 	<?php wp_head(); ?>
 </head>
