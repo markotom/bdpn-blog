2c00e83c7c5992309f8b189c1fb157f1b9c6436a
Marco Godínez <markotom@gmail.com>
2015-03-25 02:48:05 -0600

    Added index.php

--- index.php
+++ index.php
@@ -1,8 +1,46 @@
 <?php get_header(); ?>
+
+<!-- .container -->
 <div class="container">
-	<h2>Obras con más actividad</h2>
-	<p>
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis ut porro, sequi possimus doloribus facere itaque, voluptatum deserunt voluptas aliquam tenetur excepturi error. In rem explicabo, nostrum iusto dolor dicta.
-	</p>
-</div>
+
+	<!-- .col-(3|2|1)-(left|right|full) -->
+	<div class="<?php bdpn_current_layout(); ?>">
+
+		<!-- .content -->
+		<div class="content" role="main">
+			<?php
+
+				if ( have_posts() ) :
+
+					echo '<div class="post-loop">';
+
+					// Start the loop
+					while ( have_posts() ) : the_post();
+
+						// Get template content
+						get_template_part( 'templates/content' );
+
+					endwhile;
+
+					echo '</div>';
+
+					bdpn_pagination();
+
+				else :
+
+					// Get "No post found" template
+					get_template_part( 'templates/content', 'none' );
+
+				endif;
+
+			?>
+		</div><!-- /.content -->
+
+		<?php bdpn_main_sidebar(); ?>
+		<?php bdpn_secondary_sidebar(); ?>
+
+	</div><!-- /.col-(3|2|1)-(left|right|full) -->
+
+</div><!-- /.container -->
+
 <?php get_footer(); ?>
