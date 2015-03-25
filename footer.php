4f07955b8acd638a0ec3a7fa914e673ba60fff53
Marco Godínez <markotom@gmail.com>
2015-03-25 01:40:26 -0600

    Added sticky footer with content and styles

--- footer.php
+++ footer.php
@@ -1,3 +1,11 @@
+		<!-- #footer -->
+		<div id="footer">
+			<!-- .container -->
+			<div class="container">
+				Copyright © <?php echo date( 'Y' ); ?>. <?php bloginfo( 'sitename' ); ?>.
+			</div><!-- /.container -->
+		</div><!-- /#footer -->
+
 		<?php wp_footer(); ?>
 	</body>
 </html>
