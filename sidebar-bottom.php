74ea62ae5969256711777e900800b004637c7e8e
Marco Godínez <markotom@gmail.com>
2015-03-25 02:15:02 -0600

    Added sidebar files (main and bottom sidebar)

---
+++ sidebar-bottom.php
@@ -0,0 +1,14 @@
+<?php if ( is_active_sidebar( 'bottom-sidebar' ) ) : ?>
+
+<!-- .sidebar.sidebar-bottom -->
+<div class="sidebar sidebar-bottom" role="complementary">
+	<!-- .container -->
+	<div class="container">
+		<!-- .row -->
+		<div class="row">
+			<?php dynamic_sidebar( 'bottom-sidebar' ); ?>
+		</div><!-- /.row -->
+	</div><!-- /.container -->
+</div><!-- /.sidebar.sidebar-bottom -->
+
+<?php endif; ?>
