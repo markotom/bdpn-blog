b46e0da3b474c2a0c889801a312894b083bda269
Marco Godínez <markotom@gmail.com>
2015-03-25 02:33:16 -0600

    Added page content template

---
+++ templates/content-page.php
@@ -0,0 +1,14 @@
+<!-- #post-? -->
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<!-- .entry-header -->
+	<header class="entry-header">
+		<h1 class="h3 entry-title"><?php the_title(); ?></h1>
+	</header><!-- /.entry-header -->
+
+	<!-- .entry-content -->
+	<div class="entry-content">
+		<?php the_content(); ?>
+	</div><!-- /.entry-content -->
+
+</article><!-- /#post-? -->
