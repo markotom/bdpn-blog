009cf690e0ee8633432cee03441989e688a89cdc
Marco Godínez <markotom@gmail.com>
2015-03-25 00:51:55 -0600

    Added header from bdpn app with bootstrap and less
    Set non-responsive styles too

--- header.php
+++ header.php
@@ -13,3 +13,42 @@
 </head>
 
 <body <?php body_class(); ?>>
+
+	<header id="header">
+		<div class="container">
+			<div class="logo-unam pull-right">
+				<a href="http://www.unam.mx">
+					<img src="http://www.bdpn.unam.mx/images/unam_bdpn_unam.png" alt="">
+				</a>
+			</div>
+			<div class="logo pull-left">
+				<a href="<?php echo home_url() ; ?>">
+					<img src="http://www.bdpn.unam.mx/images/unam_bdpn_bgd_logo.png" alt="">
+				</a>
+			</div>
+			<h1 class="title">
+				<a href="<?php echo home_url() ; ?>"><?php bloginfo( 'sitename' ) ?></a>
+			</h1>
+			<h2 class="subtitle"><?php bloginfo( 'description' ) ?></h2>
+
+			<!-- TODO: make a wordpress menu -->
+			<ul id="nav-main" class="list-unstyled">
+				<li class="ob"><a href="http://www.bdpn.unam.mx/books">Obras</a></li>
+				<li class="au"><a href="http://www.bdpn.unam.mx/authors">Autores</a></li>
+				<li class="co"><a href="http://www.bdpn.unam.mx/collections">Colecciones</a></li>
+				<li class="in"><a href="http://www.bdpn.unam.mx/terms">Índices</a></li>
+			</ul>
+		</div>
+	</header>
+
+	<nav class="navbar navbar-inverse navbar-static-top" id="navbar-main">
+		<div class="container">
+			<!-- TODO: make a wordpress menu -->
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="#">Home</a></li>
+				<li><a href="#">Link</a></li>
+				<li><a href="#">Link</a></li>
+				<li><a href="#">Link</a></li>
+			</ul>
+		</div>
+	</nav>
--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php get_header(); ?>
+<div class="container">
+	<h2>Obras con más actividad</h2>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis ut porro, sequi possimus doloribus facere itaque, voluptatum deserunt voluptas aliquam tenetur excepturi error. In rem explicabo, nostrum iusto dolor dicta.
+	</p>
+</div>
 <?php get_footer(); ?>
